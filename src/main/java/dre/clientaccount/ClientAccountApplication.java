4b378bbd4171b07632f15fe6397398552b7ac5d8
dre36dre <dre.son36@gmail.com>
2024-08-20 21:00:03 -0300

    First commit

--- src/main/java/dre/clientaccount/ClientAccountApplication.java
+++ src/main/java/dre/clientaccount/ClientAccountApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClientAccountApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClientAccountApplication.class, args);
 	}
